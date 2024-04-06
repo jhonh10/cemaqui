aa33e2b6c23891e7e2da5158c0997283e70f5f67
Jhon Palacios <jhonhpalacios09@gmail.com>
2024-04-05 21:54:26 -0500

    change whatsapp color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         primary: "#e8ea38",
         secondary: "#eef0f3",
         tertiary: "#FF8C00",
-        whatsapp: "#25D366",
+        whatsapp: "#075E54",
         dark: "#000000",
         light: "#FFFFFF",
       },
