e6784ac528600f62f4601271b5eb7a10c0a91ad7
Jhon Palacios <jhonhpalacios09@gmail.com>
2024-04-10 13:04:16 -0500

    add sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    icon({
-      include: {
-        tabler: ["*"],
-      },
-    }),
-    tailwind(),
-  ],
+  integrations: [icon({
+    include: {
+      tabler: ["*"]
+    }
+  }), tailwind(), sitemap()]
 });
